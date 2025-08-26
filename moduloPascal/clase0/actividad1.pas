7317f1d975bce4c000c4f000f5d330487a5c34b7
GustavoVasquez <tavovm24@hotmail.com>
2025-08-25 22:24:52 -0300

    Taller de programación UNLP

---
+++ moduloPascal/clase0/actividad1.pas
@@ -0,0 +1,38 @@
+program actividad1;
+procedure generarAleatorio();
+begin
+  writeln(random(100));
+end;
+procedure generarNaleatorios(n, a, b:integer);
+var i:integer;
+begin
+  for i:=1 to n do
+    writeln(random(b - a + 1) + a);
+end;
+procedure incisoE(a, b, f:integer);
+var 
+  sigue:boolean;
+  num:integer;
+begin
+  sigue:=true;
+  while(sigue) do begin
+    num:=random(b - a + 1) + a;
+    if(num <> f) then
+      writeln(num)
+    else
+      sigue:=false;
+  end;
+end;
+var n, a, b, f, i:integer;
+begin
+  randomize;
+  for i:=1 to 20 do
+    generarAleatorio();
+  writeln('Ingrese la cantidad de veces que quiere generar de numeros random: '); readln(n);
+  writeln('Ingrese el numero de rango inferior: '); readln(a);
+  writeln('Ingrese el numero de rango superior: '); readln(b);
+  generarNaleatorios(n, a, b);
+  writeln('Ingrese el numero F: '); readln(f);
+  incisoE(a, b, f);
+end.
+ 
