95960612746377c52c30226f7e216b20bba86840
GustavoVasquez <tavovm24@hotmail.com>
2025-09-27 20:26:50 -0300

    tema1 finalizado

--- objetos/TP_ProyectoAlumnos/src/tema1/Ej03Matrices.java
+++ objetos/TP_ProyectoAlumnos/src/tema1/Ej03Matrices.java
@@ -8,11 +8,13 @@ package tema1;
 //Paso 1. importar la funcionalidad para generar datos aleatorios
 
 import PaqueteLectura.GeneradorAleatorio;
+import PaqueteLectura.Lector;
 
 public class Ej03Matrices {
 
     public static void main(String[] args) {
             int i, j;
+            int sumaFila1=0;
         //Paso 2. iniciar el generador aleatorio     
             GeneradorAleatorio.iniciar();
         //Paso 3. definir la matriz de enteros de 5x5 e iniciarla con nros. aleatorios 
@@ -28,12 +30,45 @@ public class Ej03Matrices {
                      System.out.println("-");
             }
         //Paso 5. calcular e informar la suma de los elementos de la fila 1
-            
+            for(i=0; i < 5; i++)
+               sumaFila1=sumaFila1 +  matriz[0][i];
+            System.out.println("La suma de los elementos de la fila 1 de la matriz es: " + sumaFila1);
         //Paso 6. generar un vector de 5 posiciones donde cada posición j contiene la suma de los elementos de la columna j de la matriz. 
         //        Luego, imprima el vector.
-
+            int [] vector=new int[5];
+           for (j = 0; j < 5; j++) { // j es la columna
+              for (i = 0; i < 5; i++) { // i es la fila
+                vector[j] += matriz[i][j];
+              }
+           }
+            System.out.println("----vector-----");
+            for(i=0; i<5; i ++)
+                System.out.println("Suma Columna" +  i + ": " + vector[i] + ", ");
         //Paso 7. lea un valor entero e indique si se encuentra o no en la matriz. En caso de encontrarse indique su ubicación (fila y columna)
         //   y en caso contrario imprima "No se encontró el elemento".
-
+           System.out.println("----------------");
+           System.out.println("Ingrese un numero entero desde teclado: ");
+           int value=Lector.leerInt();
+           int fila=0;
+           int col=0;
+           i=0;
+           boolean encontre=false;
+           while (i < 5 && !encontre) {
+             j = 0;
+             while (j < 5 && !encontre) {
+               if (matriz[i][j] == value) {
+                 encontre = true;
+                 fila = i;
+                 col = j;
+               }
+               j++;
+             }
+             i++;
+           }           
+           if(encontre){
+               System.out.println("La posicion del valor encontrado en la matriz es:" + "(" + fila + "," +  col +")");
+           }else{
+               System.out.println("No se encontro el elemento");
+           }
     }
 }
